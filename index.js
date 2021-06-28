1ac19b04ef60b7cd6ec0b1602c0d245f5ff6af5e
Joe Christensen <joechristensenwork@gmail.com>
2021-06-28 10:39:33 -0700

    commit tree 767c972129f94f8275e04a257d736b92b03556fe

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = function(num) {
+    return Math.abs(42-num);
+}
+
+const distanceFromHqInFeet = function(num) {
+    return distanceTravelledInFeet(num, 42);
+}
+
+const distanceTravelledInFeet = function(beginning, end) {
+    return (Math.abs(end-beginning)) * 264;
+}
+
+const calculatesFarePrice = function(start, destination) {
+    const dist = distanceTravelledInFeet(start, destination);
+    if(dist <= 400) {
+        return 0;
+    }
+    else if(dist > 400 && dist < 2000) {
+        return (dist-400) * 0.02;
+    }    
+    else if(dist >= 2000 && dist < 2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
